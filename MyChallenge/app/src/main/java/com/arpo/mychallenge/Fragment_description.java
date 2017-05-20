31d86aed7d3a153ad4377b127eda98d6f8562e67
paul <paulshaji99@gmail.com>
2017-05-20 18:41:14 +0530

    First page ui intergration, grid view onclick listner is added

--- MyChallenge/app/src/main/java/com/arpo/mychallenge/Fragment_description.java
+++ MyChallenge/app/src/main/java/com/arpo/mychallenge/Fragment_description.java
@@ -1,34 +1,23 @@
 package com.arpo.mychallenge;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Fragment_Pushuplist.OnFragmentInteractionListener} interface
+ * {@link Fragment_description.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Fragment_Pushuplist#newInstance} factory method to
+ * Use the {@link Fragment_description#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_Pushuplist extends Fragment implements AdapterView.OnItemClickListener {
-
-
+public class Fragment_description extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -40,14 +29,7 @@ public class Fragment_Pushuplist extends Fragment implements AdapterView.OnItemC
 
     private OnFragmentInteractionListener mListener;
 
-
-    private GridView gridv;
-    List<pushupc> list1;
-    Databasepushup db;
-    Adapterpushuplist adapterlist;
-
-
-    public Fragment_Pushuplist() {
+    public Fragment_description() {
         // Required empty public constructor
     }
 
@@ -57,11 +39,11 @@ public class Fragment_Pushuplist extends Fragment implements AdapterView.OnItemC
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Pushuplist.
+     * @return A new instance of fragment Fragment_description.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment_Pushuplist newInstance(String param1, String param2) {
-        Fragment_Pushuplist fragment = new Fragment_Pushuplist();
+    public static Fragment_description newInstance(String param1, String param2) {
+        Fragment_description fragment = new Fragment_description();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -76,87 +58,14 @@ public class Fragment_Pushuplist extends Fragment implements AdapterView.OnItemC
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
-    }
-
-    private void getpushuplist() {
-        list1 = new ArrayList<>();
-
-        db = new Databasepushup(getContext());
-        db.openConnection();
-
-        {
-
-            String se = "select * from tb_pushupdetails";
-
-            Cursor c = db.selectData(se);
-            if (c != null) {
-                while (c.moveToNext()) {
-                    String id = c.getString(0);
-                    String n = c.getString(1);
-                    int img = c.getInt(2);
-                    pushupc p1 = new pushupc();
-                    p1.setName(n);
-                    p1.setId(id);
-                    p1.setImage(img);
-                    list1.add(p1);
-                    Log.d("JKS", "Adding " + n);
-
-                }
-            }
-            adapterlist = new Adapterpushuplist(getContext(), list1);
-            gridv.setAdapter(adapterlist);
-            /*int count = c.getCount();*/
-            gridv.setNumColumns(2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View rootView = inflater.inflate(R.layout.fragment_fragment__pushuplist, container, false);
-        gridv = (GridView) rootView.findViewById(R.id.gv);
-        gridv.setNumColumns(2);
-
-        getpushuplist();
-        Log.d("pss", "layoutdone");
-
-        gridv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Log.d("JKS","CLicked in postion "+position);
-                try {
-
-                    Log.d("JKS","got to next fragment");
-                    Fragment nextFrag = Fragment_description.class.newInstance();
-                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.flContent, nextFrag, "description")
-                            .addToBackStack(null)
-                            .commit();
-
-                }
-                catch (Exception e)
-                {
-                    Log.d("JKS","Exception");
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
-        return rootView;
-
+        return inflater.inflate(R.layout.fragment_fragment_description, container, false);
     }
-
-
-
-
-
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -182,11 +91,6 @@ public class Fragment_Pushuplist extends Fragment implements AdapterView.OnItemC
         mListener = null;
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-    }
-
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -201,6 +105,4 @@ public class Fragment_Pushuplist extends Fragment implements AdapterView.OnItemC
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
-
 }
